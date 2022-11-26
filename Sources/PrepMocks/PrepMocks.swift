ecf161115522677d7b28a47f7bf2b4ac50831251
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-26 22:34:15 +0500

    tag increment

--- Sources/PrepMocks/PrepMocks.swift
+++ Sources/PrepMocks/PrepMocks.swift
@@ -6,59 +6,15 @@ public struct PrepMocks {
 
     public init() {
         let start = CFAbsoluteTimeGetCurrent()
-//
-//        let carrot = FoodMock.carrots
-//        let wheyProtein = FoodMock.wheyProtein
-//
-//        var meal = MealMock.preWorkoutEmpty
-//
-//        let carrotFoodItem = FoodItem(
-//            id: UUID(),
-//            food: carrot,
-//            parentFood: nil,
-//            meal: meal,
-//            amount: FoodValue(
-//                value: 50,
-//                unitType: .weight,
-//                weightUnit: .g,
-//                volumeExplicitUnit: nil,
-//                sizeUnitId: nil,
-//                sizeUnitVolumePrefixExplicitUnit: nil
-//            ),
-//            markedAsEatenAt: nil,
-//            sortPosition: 0,
-//            syncStatus: .notSynced,
-//            updatedAt: 0,
-//            deletedAt: nil
-//        )
-//        let wheyFoodItem = FoodItem(
-//            id: UUID(),
-//            food: wheyProtein,
-//            parentFood: nil,
-//            meal: meal,
-//            amount: FoodValue(
-//                value: 30.4,
-//                unitType: .weight,
-//                weightUnit: .g,
-//                volumeExplicitUnit: nil,
-//                sizeUnitId: nil,
-//                sizeUnitVolumePrefixExplicitUnit: nil
-//            ),
-//            markedAsEatenAt: nil,
-//            sortPosition: 0,
-//            syncStatus: .notSynced,
-//            updatedAt: 0,
-//            deletedAt: nil
-//        )
-//
-//        meal.foodItems = [
-//            MealFoodItem(from: carrotFoodItem),
-//            MealFoodItem(from: wheyFoodItem),
-//        ]
-//        writeEncodableToJSON(meal, type: "Meal")
-
-        var meal = MealMock.preWorkoutWithItems
-
+        
+        let carrot = FoodMock.carrots
+        guard let quantityWeWant = FoodQuantity(1, .tablespoonUS, "chopped4", carrot) else { return }
+        guard let scaleFactor = carrot.nutrientScaleFactor(for: quantityWeWant) else {
+            print("We couldn't get the scale factor")
+            return
+        }
+        ///88.5921875
+        
         print("We here, took: \(CFAbsoluteTimeGetCurrent()-start)s")
     }
 }
